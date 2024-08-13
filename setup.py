87a31953de9f3e6e5ddeb299c8dd31029a0c62bd
Jack Manners <mann0242@flinders.edu.au>
2024-08-13 09:36:33 +0930

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yawn-api',
-    version='0.1.1',
+    version='0.1.2',
     author='Jack Manners',
     author_email='jack.manners@flinders.edu.au',
     description='A Python package for interacting with the SNAPI API, YawnLabs, and various other health device APIs.',
